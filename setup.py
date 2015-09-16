e0265b1f5855f7f835a9426911e3753640f3e617
Bruno Rocha <rochacbruno@gmail.com>
2015-09-16 09:59:11 -0300

    pin flask-login version because of compatibility break

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ class PyTest(TestCommand):
 
 setup(
     name='quokka-flask-security',
-    version='1.7.4dev1',
+    version='1.7.4dev2',
     url='https://github.com/quokkaproject/flask-security',
     license='MIT',
     author='Matt Wright',
